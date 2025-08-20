207d9fb61c4c5478feed064aa7e7932b90ab927b
Leonam Silva <leonam.murilo@gmail.com>
2025-08-20 11:57:29 -0300

    test: create invalid data test

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,8 +1,10 @@
 describe("Login", () => {
-  it("Deve logar com sucesso", () => {
+  beforeEach(() => {
     cy.viewport(1440, 900);
     cy.visit("http://localhost:3000/");
+  });
 
+  it("Deve logar com sucesso", () => {
     cy.get("#email").type("papito@webdojo.com");
     cy.get("#password").type("katana123");
 
@@ -18,5 +20,27 @@ describe("Login", () => {
         "have.text",
         "Olá QA, esse é o seu Dojo para aprender Automação de Testes."
       );
+    
+    cy.url().should('include', '/dashboard')
+  });
+
+  it("Não deve logar com senha inválida", () => {
+    cy.get("#email").type("papito@webdojo.com");
+    cy.get("#password").type("654321");
+
+    cy.contains("button", "Entrar").click();
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible');
+  });
+
+  it("Não deve logar com e-mail não cadastrado", () => {
+    cy.get("#email").type("404@webdojo.com");
+    cy.get("#password").type("katana123");
+
+    cy.contains("button", "Entrar").click();
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible');
   });
 });
